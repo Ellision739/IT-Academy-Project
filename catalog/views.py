380df1dd7dd9309011317867a1d67a2c2ce9d252
Ellision739 <danila.schamov@yandex.ru>
2026-05-17 19:10:02 +0700

    feat: add base layout, book catalog view with search logic

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,35 @@
 from django.shortcuts import render
+from .models import Book, Author, BookInstance
+from django.db.models import Q
 
-# Create your views here.
+
+def index(request):
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'catalog/index.html', context=context)
+
+def book_list(request):
+    search_query = request.GET.get('search', '')
+
+    if search_query:
+        books = Book.objects.filter(
+            Q(title__icontains=search_query) |
+            Q(genre__icontains=search_query) |
+            Q(authors__name__icontains=search_query)
+        ).distinct()
+    else:
+        books = Book.objects.all()
+
+    context = {
+        'books': books,
+        'search_query': search_query,
+    }
+    return render(request, 'catalog/book_list.html', context)
